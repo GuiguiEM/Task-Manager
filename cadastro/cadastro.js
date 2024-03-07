dfce921ff5ccff7b29c9349bb98522f74ce9b8a4
GUILHERME DE OLIVEIRA ABEL <23111730@CFP127ALUNO.LOCAL>
2024-03-07 17:00:54 -0300

    369

---
+++ cadastro/cadastro.js
@@ -0,0 +1,28 @@
+'use strict'
+
+async function cadastrarUser() {
+    const nomeCadastro = document.getElementById('nomeCadastro').value
+    const emailCadastro = document.getElementById('emailCadastro').value
+    const senhaCadastro = document.getElementById('senhaCadastro').value
+
+    const newUser = {
+        nomeCadastro,
+        emailCadastro,
+        senhaCadastro
+    }
+
+    const url = 'http://localhost:8080/usuario'
+
+    const options = {
+        method: 'Post',
+        headers: {
+            'Content-type' : 'Application/json',
+        },
+        body: JSON.stringify(newUser)
+    }
+
+    fetch(url,options)
+    alert('Usuário Cadastrado com sucesso')
+    window.location.href = '../tarefas/tarefas.html'
+
+}
